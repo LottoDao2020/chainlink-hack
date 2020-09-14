f16deb74f2ae58b0d41d1debcad06ccb9c81f06d
davids1011 <davids1011@gmail.com>
2020-09-14 15:27:57 +1000

    Lottery contract update

--- contracts/lottery1.sol
+++ contracts/lottery1.sol
@@ -1,22 +1,29 @@
-pragma solidity ^0.4.21;
+pragma solidity ^0.6.0;
 
 contract Lottery {
-    mapping (address => mapping(uint8 => uint64 [][])) entries;
+    mapping (address => mapping(uint16 => uint64 [][])) entries;
+    uint32 draw_no;
     uint8 totalnum = 0;
-    uint nonce = 0;
 
-    function buy() public payable {
-        require(msg.value == 0.01 ether);
-        require(participantsCount < 10);
-        require(drawSituation(msg.sender) == false);
+	Struct draw{
+	  enum LOTTERY_STATE { OPEN, CLOSED, CALCULATING_WINNER };
+	  uint8 rewards;
+	}
+
+    mapping(uint8 => draw) draws;
+
+    event LogBuy(address current_user, uint msg.value, bytes entries);
+    event LogClaim(address current_user, uint reward);
+
+    function buy(bytes calldata _entries) public external payable {
+        uint8 prize_per_entry;
+	require(msg.value >= entries * prize_per_entry, "needs more Ether");
         results[totalnum] = msg.sender;
-        totalnum++;
-        if (totalnum == 10) {
-            claim();
-        }
+	totalnum++;
     }
 
     function validate(address _entries) private view returns(bool) {
+	mapping (address => mapping(uint16 => mapping(uint64 => mapping(uint8 => bool)))) results;
         bool winning = false;
         for(uint i = 0; i < 10; i++) {
             if (entries[i] == _entry) {
@@ -27,17 +34,35 @@ contract Lottery {
     }
     
     function claim() private returns(address) {
-        require(totalnum == 10);
-        address winner = results[winnerNumber()];
+	mapping (address => mapping(uint16 => mapping(uint64 => mapping(uint8 => bool)))) results;
+
+//        address winner = result_Chainlink_Oracle;    need to integrate with Chainlink Oracle
+
         winner.transfer(address(this).balance);
         delete entries;
         totalnum = 0;
         return winner;
     }
-    
-    function winnerNumber() private returns(uint) {
-        uint winner = uint(keccak256(abi.encodePacked(now, msg.sender, nonce))) % 10;
-        nonce++;
-        return winner;
+
+    function claim(uint32 draw_no) public external returns(uint8 amount) {
+	if (results.includes(draw_no)) {
+
+//		amount = rewards_Chainlink_Oracle;	need to integrate with Chainlink Oracle
+
+	}
+	else {
+	amount = 0;
+	}
+        return amount;
     }
+
+    function mockData (uint8 test1, uint8 test2) external public{
+	  results[test1] = test1;
+	  draws[1] = test2;
+    }
+
+    function setOracle(address _oracle) onlyOwner public external{
+    	oracle = _oracle
+    }
+    
 }
